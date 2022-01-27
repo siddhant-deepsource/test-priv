f956c702f62855904f26e78fb1c1b6211b7a38dd
Siddhant N Trivedi <siddhant@deepsource.io>
2022-01-27 13:16:37 +0530

    add dummy code with private dep
    Signed-off-by: Siddhant N Trivedi <siddhant@deepsource.io>

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/siddhant-deepsource/test-priv
+
+go 1.17
+
+require bitbucket.org/siddhant-deepsource/godirwalkfork v0.0.0-20220127070822-b5fe8968f2df
---
+++ types.go
@@ -0,0 +1,46 @@
+package main
+
+type Namespace struct {
+	Key   string  `json:"key"`
+	Value float64 `json:"value"`
+}
+
+type Metric struct {
+	MetricCode string      `json:"metric_code"`
+	Namespaces []Namespace `json:"namespaces"`
+}
+
+type Error struct {
+	HMessage string `json:"hmessage"`
+	Level    int    `json:"level"`
+}
+
+type MacroResult struct {
+	Issues    []Issue     `json:"issues"`
+	Metrics   []Metric    `json:"metrics,omitempty"`
+	IsPassed  bool        `json:"is_passed"`
+	Errors    []Error     `json:"errors"`
+	ExtraData interface{} `json:"extra_data"`
+}
+
+type Coordinate struct {
+	Line   int `json:"line"`
+	Column int `json:"column"`
+}
+
+type Position struct {
+	Begin Coordinate `json:"begin"`
+	End   Coordinate `json:"end"`
+}
+
+type Location struct {
+	Path     string   `json:"path"`
+	Position Position `json:"position"`
+}
+
+type Issue struct {
+	Code     string   `json:"issue_code"`
+	Title    string   `json:"issue_text"`
+	Location Location `json:"location"`
+}
+
